655c45e09cd86771c81d198a54f651bb33b15956
Luisinho Layme <luisinhotf@gmail.com>
2024-04-05 06:55:35 -0400

    proteccion rutas, relaciones, middlewares, paginacion

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 import { AuthRoutes } from './auth/routes';
+import { CategoryRoutes } from './category/routes';
 
 export class AppRoutes {
-
 
   static get routes(): Router {
 
@@ -10,6 +10,7 @@ export class AppRoutes {
 
     // Definir las rutas
     router.use('/api/auth', AuthRoutes.routes );
+    router.use('/api/categories', CategoryRoutes.routes );
 
 
 
@@ -18,4 +19,3 @@ export class AppRoutes {
 
 
 }
-
